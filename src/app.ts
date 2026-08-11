0fe9e1b1bb408ad39c16a946a83be7d14b3417e5
laramzfranco <laramzfranco@gmail.com>
2026-08-11 11:40:27 -0300

    projeto-backend

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+// Importa a biblioteca Express
+// O Express será utilizado para criar o servidor web
+import express = require("express");
+
+// Cria uma aplicação Express
+// A função express() devolve um objeto que representa o servidor da aplicação
+const app = express();
+
+// Define a porta onde o servidor ficará disponível
+// Neste caso, o servidor poderá ser acessado pela porta 8081
+const PORT: number = 8081;
+
+// Inicializa o servidor utilizando a porta definida
+// O método listen() faz o servidor começar a "escutar" requisições HTTP
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
